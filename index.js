d116ed0df695a77aef53480a248c2eed9488ce69
ArchersBack <scout549@hotmail.com>
2017-08-27 17:13:41 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function go () {
   var type = $('input[name=confession-type]:checked').val();
   var identity = $('#identity').val();
   var message = '#420 - ' + type + '\n\n' + confession + '\n\nSubmitted by: ' + identity;
-  var pageId = '590175910999153';
-  var token = 'EAACEdEose0cBAJypXA9zZCEXZBUur0b3SLIxJWrKoCRyrXDRx1xGH8ukCftfWcDmJZBn6CM132iL7NNxIGcZBiqGjWjnH5MxGRrknDZCtIWFDNTPNjrXDlU6bEZC8zsKmNfqA4Q1bZAuZBbz902z0SycsuKOTQqkR64aiO7Ec13YGuw9mk7TZBJERRWGnUL8ZAe7XYXLS22hvHIQZDZD';
+  var pageId = '288504841525348';
+  var token = 'EAACEdEose0cBAKGZAg2NIxC35TmWv4tBfcyXSy4ezoz7TXT3BY923SJmxwdD6Guagetl1ZBHZCYjXo6siW3rykPSn1RmvFjEF59DZAGNg1JVoZA9tR5S6cZB55oJeni6OxjmXblo7839TkLEvFw9BGyIPjmSDuQp4urrQuxu2BS4yXf9hOurBvMRTLufErHiHCbxAebwBNkAZDZD';
 
   $.ajax({
     type: 'POST',
     url: 'https://graph.facebook.com/v2.10/' + pageId + '/feed?access_token=' + token,
     data: {message: message},
     success: function(response) {
-      alert('success');
+      alert('Confession Posted');
     }
   });
 }
