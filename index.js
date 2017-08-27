f12f690155c2c7b59ae42a4e6b5e2a8f524a01c9
Myke <mykelray.perez@toro.io>
2017-08-27 15:29:28 +0800

    add filters

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function go () {
+  var pageId = '590175910999153';
+  var token = 'EAACEdEose0cBAJypXA9zZCEXZBUur0b3SLIxJWrKoCRyrXDRx1xGH8ukCftfWcDmJZBn6CM132iL7NNxIGcZBiqGjWjnH5MxGRrknDZCtIWFDNTPNjrXDlU6bEZC8zsKmNfqA4Q1bZAuZBbz902z0SycsuKOTQqkR64aiO7Ec13YGuw9mk7TZBJERRWGnUL8ZAe7XYXLS22hvHIQZDZD';
+
   var confession = $('#confession').val();
   var type = $('input[name=confession-type]:checked').val();
   var identity = $('#identity').val();
   var message = '#420 - ' + type + '\n\n' + confession + '\n\nSubmitted by: ' + identity;
-  var pageId = '590175910999153';
-  var token = 'EAACEdEose0cBAJypXA9zZCEXZBUur0b3SLIxJWrKoCRyrXDRx1xGH8ukCftfWcDmJZBn6CM132iL7NNxIGcZBiqGjWjnH5MxGRrknDZCtIWFDNTPNjrXDlU6bEZC8zsKmNfqA4Q1bZAuZBbz902z0SycsuKOTQqkR64aiO7Ec13YGuw9mk7TZBJERRWGnUL8ZAe7XYXLS22hvHIQZDZD';
+
+  message = filter(message);
 
   $.ajax({
     type: 'POST',
@@ -14,4 +17,22 @@ function go () {
       alert('success');
     }
   });
+}
+
+function filter (message) {
+  var message = message;
+  var zucc = [{
+    word: 'nigger',
+    replacement: 'n*gg*r'
+  }, {
+    word: 'faggot',
+    replacement: 'f*gg*t'
+  }];
+
+  zucc.forEach(function(z) {
+    var regex = new RegExp(z.word, 'g');
+    message = message.replace(regex, z.replacement);
+  })
+
+  return message;
 }
