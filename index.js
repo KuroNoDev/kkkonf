ae17d8f98e6221d84229ed1d26631155b6481b76
Myke <mykelray.perez@toro.io>
2017-08-27 14:58:30 +0800

    alert

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ function go () {
   var type = $('input[name=confession-type]:checked').val();
   var identity = $('#identity').val();
   var message = '#420 - ' + type + '\n\n' + confession + '\n\nSubmitted by: ' + identity;
+  var pageId = '590175910999153';
+  var token = 'EAACEdEose0cBAJypXA9zZCEXZBUur0b3SLIxJWrKoCRyrXDRx1xGH8ukCftfWcDmJZBn6CM132iL7NNxIGcZBiqGjWjnH5MxGRrknDZCtIWFDNTPNjrXDlU6bEZC8zsKmNfqA4Q1bZAuZBbz902z0SycsuKOTQqkR64aiO7Ec13YGuw9mk7TZBJERRWGnUL8ZAe7XYXLS22hvHIQZDZD';
 
   $.ajax({
     type: 'POST',
-    url: 'https://graph.facebook.com/v2.10/590175910999153/feed?access_token=EAACEdEose0cBAJypXA9zZCEXZBUur0b3SLIxJWrKoCRyrXDRx1xGH8ukCftfWcDmJZBn6CM132iL7NNxIGcZBiqGjWjnH5MxGRrknDZCtIWFDNTPNjrXDlU6bEZC8zsKmNfqA4Q1bZAuZBbz902z0SycsuKOTQqkR64aiO7Ec13YGuw9mk7TZBJERRWGnUL8ZAe7XYXLS22hvHIQZDZD',
+    url: 'https://graph.facebook.com/v2.10/' + pageId + '/feed?access_token=' + token,
     data: {message: message},
     success: function(response) {
-      console.log(response);
+      alert('success');
     }
   });
 }
